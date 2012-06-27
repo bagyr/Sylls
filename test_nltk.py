9921e3583ba504d388cce269997b42b982c81403
bagyr <bagyrthelancer@gmail.com>
2012-06-27 18:17:33 +0400

    First version

--- test_nltk.py
+++ test_nltk.py
@@ -19,29 +19,55 @@ def nsyl(word):
         return out
 
 sylls = 17
+
+pattern = [5, 7, 5]
+
+def findPattern(sentences, pattern):
+    ns = 0
+    out = []
+    for i in sent:
+        _pattern = pattern[:]
+        maxsyl = _pattern.pop()
+        tokens = nltk.word_tokenize(i)
+        for j in tokens:
+            ns += max(nsyl(j))
+            haiku.append(j)
+            if ns > maxsyl or len(_pattern) == 0:
+                haiku = []
+                ns = 0
+                break
+            elif ns == maxsyl:
+                ns = 0
+                maxsyl = _pattern.pop()
+        ns = 0
+        haiku = []
+        out.append(haiku)
+    return out
+
+
 f = open('./conv2.txt')
 text = f.read()
 sent = sent_detector.tokenize(text.strip())
 sent = [x.strip() for x in sent[:]]
 print len(sent)
-ns = 0
-out = []
-haiku = 0
-for i in sent:
-    tokens = nltk.word_tokenize(i)
-    ns = 0
-    out = []
-    for j in tokens:
-        ns += max(nsyl(j))
-        out.append(j)
-        if ns > sylls:
-            ns = 0
-            out = []
-            break
-    if ns == sylls:
-        haiku += 1
-        print ' '.join(out)
-    ns = 0
-    out = []
-print 'total', haiku
+# ns = 0
+# out = []
+# haiku = 0
+# for i in sent:
+#     tokens = nltk.word_tokenize(i)
+#     ns = 0
+#     out = []
+#     for j in tokens:
+#         ns += max(nsyl(j))
+#         out.append(j)
+#         if ns > sylls:
+#             ns = 0
+#             out = []
+#             break
+#     if ns == sylls:
+#         haiku += 1
+#         print ' '.join(out)
+#     ns = 0
+#     out = []
+# print 'total', haiku
 
